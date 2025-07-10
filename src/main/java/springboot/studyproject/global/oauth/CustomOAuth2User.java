810395ba9421c4cb52c4e6db56b10ab4f95bd45b
HyunSeung <lleen0114@naver.com>
2025-07-10 23:40:29 +0900

    Feat: CustomOAuth2User 추가

---
+++ src/main/java/springboot/studyproject/global/oauth/CustomOAuth2User.java
@@ -0,0 +1,39 @@
+package springboot.studyproject.global.oauth;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import springboot.studyproject.domain.user.entity.Users;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User implements OAuth2User {
+
+    private final Users user;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(Users user, Map<String, Object> attributes){
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes(){
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("ROLE_User"));
+    }
+
+    @Override
+    public String getName(){
+        return user.getUserEmail();
+    }
+
+}
