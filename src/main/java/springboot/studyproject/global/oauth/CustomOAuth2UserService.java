180414f04088749ea842f6acc1c662af51a9d9cb
HyunSeung <lleen0114@naver.com>
2025-07-13 23:05:26 +0900

    Feat: CustomOAuth2UserService 추가

---
+++ src/main/java/springboot/studyproject/global/oauth/CustomOAuth2UserService.java
@@ -0,0 +1,25 @@
+package springboot.studyproject.global.oauth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import springboot.studyproject.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
+        //OAuth2UserService로부터 사용자 정보 획득
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+
+    }
+
+}
